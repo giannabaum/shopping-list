ac4383b82ef2b9d4f653ecc7979b33ac7be8d477
giannabaum <68743997+giannabaum@users.noreply.github.com>
2020-08-15 18:38:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 $(function() {
-  //listens for when a the form is submitted
+  //listens for when the form is submitted
   //prevents the form from resubmitting
     $('#js-shopping-list-form').submit(function(event){
       event.preventDefault();
       
       //create a new variable from the value entered into the form
-      //create a new variable of the HTML list information with the newItem information
+    
         var newItem= $('#shopping-list-entry').val();
-        var listHtml=<li> 
-        <span class="shopping-item">${newItem}</span>
-          <div class="shopping-item-controls">
-            <button class="shopping-item-toggle">
-              <span class="button-label">check</span>
-            </button>
-            <button class="shopping-item-delete">
-              <span class="button-label">delete</span>
-            </button>
-          </div>
-        </li>
-
-
+        
         //add add the new item to the list 
         //return the form value to empty so new entries can be made
-        $('.shopping-list').append(listHtml); 
+        $('.shopping-list').append(
+        `<li> 
+          <span class="shopping-item">${newItem}</span>
+            <div class="shopping-item-controls">
+              <button class="shopping-item-toggle">
+                <span class="button-label">check</span>
+              </button>
+              <button class="shopping-item-delete">
+                <span class="button-label">delete</span>
+              </button>
+            </div>
+          </li>`); 
        $('#shopping-list-entry').val('');
     });
 //create an event for when the button for "checked" is clicked
